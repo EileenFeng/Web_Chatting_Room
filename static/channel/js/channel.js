bf214f0d30976918283655f6b62c26fd9191b143
Phuc Cao <hanhphuc296@gmail.com>
2018-04-26 02:29:32 -0400

    fixing button submissions

--- static/channel/js/channel.js
+++ static/channel/js/channel.js
@@ -89,15 +89,15 @@
     });
 
     /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
+    [ Validate 1]*/
+    var input1 = $('.validate-input-1 .input100');
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form-1').on('submit',function(){
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
+        for(var i=0; i<input1.length; i++) {
+            if(validate(input1[i]) == false){
+                showValidate(input1[i]);
                 check=false;
             }
         }
@@ -106,11 +106,39 @@
     });
 
 
-    $('.validate-form .input100').each(function(){
+    $('.validate-form-1 .input100').each(function(){
         $(this).focus(function(){
             hideValidate(this);
         });
     });
+
+
+    /*==================================================================
+    [ Validate ]*/
+
+    var input2 = $('.validate-input-2 .input100');
+
+    $('.validate-form-2').on('submit',function(){
+        var check = true;
+
+        for(var i=0; i<input2.length; i++) {
+            if(validate(input2[i]) == false){
+                showValidate(input2[i]);
+                check=false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form-2 .input100').each(function(){
+        $(this).focus(function(){
+            hideValidate(this);
+        });
+    });
+
+    /*================================================================== */
 
     function validate (input) {
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
@@ -169,11 +197,10 @@ addAdminBtn.onclick = function() {
 span.onclick = function() {
     modal.style.display = "none";
 }
-*/
 
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-
+*/
